9c852e5c18819bb32f39d1ce039731ddbac07467
Jaime <Jismalt26@gmail.com>
2025-04-15 19:53:05 -0500

    Actualizacion de show,person y control

--- src/App.java
+++ src/App.java
@@ -1,7 +1,11 @@
+import Controllers.MetodosBusqueda;
+import Models.Persona;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("----------Programa funcionando----------\n");
-
+        System.out.println("\n----------Programa funcionando----------\n");
+//-------------------------------------------------------------------------------------------------------------
+        /* 
         int[] arr = { 12, -7, 25, 0, -15, 33, 19, -22, 5, 48, -3, 27, -30, 14, 7, -1, 41, 20, -11, 8 };;
         int num = 5;
 
@@ -13,6 +17,21 @@ public class App {
         mB.imprimirArreglo(arr);
 
         System.out.println("\nEl numero ("+ num +") esta en la posicion: " + pos);
-        System.out.println();
+        System.out.println(); 
+        */
+//-------------------------------------------------------------------------------------------------------------
+        Persona[] personas = new Persona[7];
+
+        personas[0] = new Persona(101, "Juan");
+        personas[1] = new Persona(102, "Maria");
+        personas[2] = new Persona(103, "Carlos");
+        personas[3] = new Persona(104, "Ana");
+        personas[4] = new Persona(105, "Luis");
+        personas[5] = new Persona(106, "Sofia");
+        personas[6] = new Persona(107, "Pedro");
+
+        MetodosBusqueda mB = new MetodosBusqueda(personas);
+
     }
+
 }
---
+++ src/Controllers/MetodosBusqueda.java
@@ -0,0 +1,70 @@
+package Controllers;
+
+import Models.Persona;
+import Views.ShowConsole;
+
+public class MetodosBusqueda {
+//-------------------------------------------------------------------------------------------------------------
+    private ShowConsole showConsole;
+    private Persona[] people;
+
+    public MetodosBusqueda(){
+
+    }
+    
+    public MetodosBusqueda(Persona[] personas){
+        showConsole = new ShowConsole();
+        this.people = personas;
+        showPersona();
+    }
+
+//-------------------------------------------------------------------------------------------------------------
+    public int busquedaLineal(int[] arr, int num) {
+        int tam = arr.length;
+
+        for (int i = 0; i < tam; i++ ) {
+            if (arr[i] == num) {
+                return i; 
+            }
+        }
+        return -1;
+    }
+//-------------------------------------------------------------------------------------------------------------
+    public void showPersona(){
+        int codeToFind = showConsole.inputCode();
+        int indexPersona = findPersonaByCode(codeToFind);
+
+        if(indexPersona >= 0){
+            showConsole.showMessage("Persona con codigo " + indexPersona + " encontrada.");
+            showConsole.showMessage(people[indexPersona].toString());   
+                                                      //.toString necesario para q salga
+
+            //showConsole.showMessage((String)people[indexPersona]); 
+            //showConsole.showMessage(people[indexPersona] + "");   
+        }else{
+            showConsole.showMessage("Persona con codigo " + indexPersona + " no encontrada.");
+        }
+    }
+//-------------------------------------------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------------------------------------------
+    public  int findPersonaByCode(int code){
+        int tam = people.length;
+
+        for (int i = 0; i < tam; i++ ) {
+            if (people[i].getCode() == code) {
+                return i; 
+            }
+        }
+        return -1;
+    }
+//-------------------------------------------------------------------------------------------------------------
+    public void imprimirArreglo(int[] arreglo){
+        int tam = arreglo.length;
+    
+        for(int i = 0; i < tam; i ++){   
+            System.out.print( arreglo[i] + " / ");
+        }
+        System.out.println();
+    }
+}
